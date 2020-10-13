085f43bc5b7bf1e1e7c19ceae3c3ae3c45c69cce
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-10-13 16:40:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 !== 0 || str.length < 2) return false;
+    else {
+      for(let i = 0; i < bracketsConfig.length;) {
+          let bracketsType = bracketsConfig[i].join('');
+          if(str.includes(bracketsType )){
+            str = str.replace(bracketsType, '');
+            i = 0;
+        } else {
+            i++;
+        }     
+        }
+        if(str.length) return false;
+        else return true;
+  }      
 }
