90e92a495cc1488f7c85a2f2d8ef3be833d47498
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-10-13 16:46:33 +0300

    fix: add comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
+  //проверяем на четное количество символы (парные или нет) и на пустую строку
   if(str.length % 2 !== 0 || str.length < 2) return false;
-    else {
+    else {      
       for(let i = 0; i < bracketsConfig.length;) {
-          let bracketsType = bracketsConfig[i].join('');
+          //проверяем строку на наличие пары скобок первого типа из второго аргумента
+          let bracketsType = bracketsConfig[i].join('');          
           if(str.includes(bracketsType )){
+            //удаляем совпадения и дальше проверяем строку на наличие скобок этого же типа, обнуляя счетчик
             str = str.replace(bracketsType, '');
             i = 0;
+            //если совпадений больше нет - переходим к следующему типу скобок
         } else {
             i++;
         }     
         }
+        //если символы в строке остались - значит скобки закрыты неправильно
         if(str.length) return false;
         else return true;
   }      
